e32db7beb5cf97626850a1c8c80f4c2fbbfda5a0
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2026-02-13 01:49:06 +0700

    set up project youapp backend

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('YouApp API')
+    .setDescription('Backend Technical Test')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
