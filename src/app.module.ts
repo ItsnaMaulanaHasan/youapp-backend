d945a18971edb603ae11163e8f31d4aadb543fa8
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2026-02-15 00:38:53 +0700

    fix: app module for get jwt secret on env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,8 @@ import { AppService } from './app.service';
     }),
 
     MongooseModule.forRoot(process.env.MONGO_URI as string),
+
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
